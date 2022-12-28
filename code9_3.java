4fb17e51cbb4baaf34e8aa976bad2c7ada2bdf97
OritoYumeka <119559660+OritoYumeka@users.noreply.github.com>
2022-12-28 15:35:01 +0900

    Add files via upload

---
+++ code9_3.java
@@ -0,0 +1,20 @@
+package kadai1;
+public class code9_3 {
+    public static void main(String[]args){
+        void keyPressed(){
+            if(key=='s'){//鏡映
+                j*=-1;
+                println(key);
+            }else if(key=='r'){//回転
+                k=(k+j+gon)%gon;
+                println(key);
+            }else if(key=='e'){//初期化
+                k=0;
+                j=1;
+                println("RESET");
+            }
+            drawShape();
+        }
+        void draw(){}   
+    }
+}
