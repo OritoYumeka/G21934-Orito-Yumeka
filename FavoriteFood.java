8de1ed9d48fc009cbdc5c629a6ee538b23ca9d3c
OritoYumeka <119559660+OritoYumeka@users.noreply.github.com>
2023-04-21 16:37:07 +0900

    Add files via upload

---
+++ FavoriteFood.java
@@ -0,0 +1,7 @@
+package kadai1;
+public class FavoriteFood {
+    public static void main(String[]args){
+        System.out.println("好きな食べ物:夕張メロン");
+        System.out.println("理由:酸味が強くて、果汁がたっぷり入っているから。");
+    }
+}
